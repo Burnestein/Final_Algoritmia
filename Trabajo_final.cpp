baafe274b4f060626a66617d8dbd2c8742c08408
Burnestein <burn_nemesis@hotmail.com>
2021-05-29 12:55:13 -0500

    Hola mundo
    Inicializo programa con funcion main

---
+++ Trabajo_final.cpp
@@ -0,0 +1,18 @@
+/*
+Alumno: Nemesis J. Sierra Aranda 221404306
+Profesor: Jos� Francisco Reinaga Camacho
+Carrera: Ingenier�a en Computaci�n, 1er Semestre
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout<<"TRABAJO FINAL ALGORITMIA"<<endl;
+	cout<<endl<<"Seleccione uno de los siguientes ejercicios:"<<endl;
+	cout<<"1. Hola mundo"<<endl;
+
+
+	return(0);
+}
+
